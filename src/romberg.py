4d1a9acac9db2c267912d456bd15292963072e80
Matheus Alano Joenck <matheusalanojoenck@gmail.com>
2021-03-07 21:01:23 -0300

    Integral - Método de Romberg

---
+++ src/romberg.py
@@ -0,0 +1,41 @@
+# basta aplicar o metodo de extrapolaçao de Richardson sobree a formula da Regra dops Trapwzios
+
+import math
+
+def trapz(f, a, b, h):
+
+    soma = 0
+    n = int((b - a) / h)
+    for k in range(1, n):
+        soma += f(a + k * h)
+    return (h/2) * (f(a) + 2 * soma + f(b))
+
+def romberg(col1):
+    col1 = [item for item in col1] # copia coluna 1
+    n = len(col1)
+    for j in range(n - 1):
+        temp_col = [0] * (n - 1 - j)
+        for i in range(n - 1 - j):
+            power = j + 1 # é igual o numero da coluna anterior
+            temp_col[i] = (4 ** power * col1[i + 1] - col1[i]) / (4 ** power - 1)
+        col1[:n - 1 - j] = temp_col
+        print(f'F_{j+2}', temp_col)
+    return col1[0] # a aproximacao procurada
+
+def f(x):
+    return math.exp(-x ** 2)
+
+if __name__ == '__main__':
+
+    a, b = [-0.5, 2]
+
+    h = 0.5
+    n = 5
+    hs = [h/2 ** i for i in range(n)]
+
+    col1 = [trapz(f, a, b, hi) for hi in hs]
+    print('F_1', col1)
+    r = romberg(col1)
+    s = f'o numero {r} é uma aproximacao  para integral de exp(-x^2) em [{a}, {b}] com erro O(h^2(2*{n}))'
+    print(s)
+
