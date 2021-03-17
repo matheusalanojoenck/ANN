a3683bfba12d6acdca18e18840081d2f39b98a94
Matheus Alano Joenck <matheusalanojoenck@gmail.com>
2021-03-17 14:29:41 -0300

    Questões Spline | Trabalho 2

--- src/spline.py
+++ src/spline.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
 def spline(x, y):
     """
@@ -35,25 +36,43 @@ def spline(x, y):
 
     s = {}
     for k in range(n - 1):
+        #print(f'a{k} = {a[k]} | b{k} = {b[k]} | c{k} = {c[k]} | d{k} = {d[k]}')
         eq = f'{a[k]}{b[k]:+}*(x{-x[k]:+}){c[k]:+}*(x{-x[k]:+})**2{d[k]:+}*(x{-x[k]:+})**3'
         s[k] = {'eq': eq, 'domain': [x[k], x[k+1]]}
 
     return s
 
+def f(x):
+    return math.exp(-x ** 2) + math.cos(x) + 3
+
+
 if __name__ == '__main__':
 
-    x = [1, 2, 4, 5]
-    y = [1, 4, 2, 3]
+    pontos = [[-7.025, 1.679], [-5.383, 3.818], [-3.077, 3.62], [-1.387, 2.953], [-0.076, 3.924], [1.966, 4.794], [2.303, 4.567], [4.621, 2.292], [5.89, 2.46], [7.713, 3.007], [9.652, 0.669], [11.965, -1.338], [12.886, -1.221]]
+    x = [0] * len(pontos)
+    y = [0] * len(pontos)
+    for i in range(len(pontos)):
+        x[i] = pontos[i][0]
+        y[i] = pontos[i][1]
+
+    # x = [0.283, 0.834, 1.305, 1.893, 2.872, 3.162, 3.881, 4.439, 5.027, 5.264, 6.173, 6.953]
+    # y = [0] * len(x)
+    # for i in range(len(x)):
+    #     y[i] = f(x[i])
 
     eqs = spline(x, y)
-    print(eqs)
+
+    xn = [-4.719, -4.403, -1.99, -0.784, 11.161]
 
     for key, value in eqs.items():
         def p(x):
             return eval(value['eq'])
         t = np.linspace(*value['domain'], 100)
         plt.plot(t, p(t), label=f"$S_{key}(x)$")
-
+        for i in range(len(xn)):
+            if xn[i] > value['domain'][0] and xn[i] < value['domain'][1]:
+                #print(i, 'equaçao', ' | ponto:  ',  xn[i], '| domain', value['domain'])
+                print(p(xn[i]))
 
     plt.scatter(x, y)
     plt.legend()
